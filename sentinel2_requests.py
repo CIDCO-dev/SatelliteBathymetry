0b1f5c1aa7765f24a0ca675288b952814bb035a1
Samuel DUBOS <80334118+SamuelDubos@users.noreply.github.com>
2022-05-19 15:03:36 -0400

    Update sentinel2_requests.py

--- sentinel2_requests.py
+++ sentinel2_requests.py
@@ -1,13 +1,12 @@
 """
-
 Copyright 2022 © Centre Interdisciplinaire de développement en Cartographie des Océans (CIDCO), Tous droits réservés
-
 @Samuel_Dubos
-
+@Glm
 """
 
-import requests as rq
+import requests
 import sys
+import os
 
 
 class Sentinel2(object):
@@ -16,58 +15,62 @@ class Sentinel2(object):
         self.username = username
         self.password = password
 
-    def search(self, date, footprint, length=10, step=1):
+    def search(self, begin_date, end_date, footprint):
 
         ids = []
-        for i in range(step):
-            req = rq.get(f'https://scihub.copernicus.eu/dhus'
-                         f'/search?'
-                         f'format=json'
-                         f'&start={i * length}&rows={length}'
-                         f'&q=(footprint:%22Intersects({footprint})%22)'
-                         f'%20AND%20(beginPosition:[{date}%20TO%20NOW])'
-                         f'%20AND%20(platformname:Sentinel-2'
-                         f'%20AND%20filename:S2A_*'
-                         f'%20AND%20producttype:S2MSI2A)',
-                         auth=(self.username, self.password))
+        page = 0
+        resultsPerPage = 10
+        totalResults = 100 #anything gt resultsPerPage will do
+
+        # get a page of search results
+        while len(ids) < totalResults:
+            req = requests.get(f'https://scihub.copernicus.eu/dhus'
+                             f'/search?'
+                             f'format=json'
+                             f'&start={page * resultsPerPage}&rows={resultsPerPage}'
+                             f'&q=(footprint:%22Intersects({footprint})%22)'
+                             f'%20AND%20(beginPosition:[{begin_date}%20TO%20{end_date}])'
+                             f'%20AND%20(platformname:Sentinel-2'
+                             f'%20AND%20producttype:S2MSI2A)',
+                             auth=(self.username, self.password))
 
             if req.status_code == 200:
                 json_data = req.json()
-                if 'entry' in json_data['feed'].keys():
-                    for j, answer in enumerate(json_data['feed']['entry']):
-                        ids.append(answer['id'])
-                        print(f'{100*(i*length+j)/(step*length)}%')
-                else:
-                    return ids
-            else:
-                sys.stderr.write(f"Error while fetching file search results: {req.status_code}")
+                totalResults = int(json_data['feed']['opensearch:totalResults'])
 
+                if totalResults != 0 and json_data['feed']['entry']:
+                    # Process received data
+                    list_dic = json_data['feed']['entry']
+                    k = 0
+                    flag = True
+                    while k < len(list_dic) and flag:
+                        (result, flag) = (list_dic['id'], False) if 'id' in list_dic else (list_dic[k]['id'], True)
+                        ids.append(result)
+                        k += 1
+                    page += 1
+                else:
+                    sys.stderr.write("Error: No entries in search results\n")
+                    break
+
+            else:
+                sys.stderr.write(f"Error while fetching file search results: {req.status_code}\n")
+                break
+            
         return ids
 
     def download(self, id):
 
-        req = rq.get(f"https://scihub.copernicus.eu/dhus"
+        req = requests.get(f"https://scihub.copernicus.eu/dhus"
                      f"/odata/v1/Products('{id}')"
                      f"/$value",
                      auth=(self.username, self.password))
 
         if req.status_code == 200:
-            f = open(f'{id}.zip', 'wb')
+            #Save binary data to ZIP file
+            f = open(f'{os.path.dirname(os.path.abspath(os.path.realpath(__file__)))}/{id}.zip', 'wb')
             f.write(req.content)
             f.close()
         else:
-            sys.stderr.write(f"Error while fetching file search results: {req.status_code}")
+            sys.stderr.write(f"Error while fetching file search results: {req.status_code}\n")
 
-        return 0
-
-
-if __name__ == "__main__":
-
-    # Parameters definition as an example
-    example_date = '2022-05-15T18:30:00.000Z'
-    example_footprint = 'POLYGON((' \
-                '-69.87041507596486 51.67827946383208,' \
-                '-57.21416507596486 51.67827946383208,' \
-                '-57.21416507596486 47.52343468727058,' \
-                '-69.87041507596486 47.52343468727058,' \
-                '-69.87041507596486 51.67827946383208))'
+        return req.content
