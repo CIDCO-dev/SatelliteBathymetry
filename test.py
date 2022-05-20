08fbbd141e67a23235c2e7b777f2ef89771a19ce
Samuel DUBOS <80334118+SamuelDubos@users.noreply.github.com>
2022-05-20 16:13:55 -0400

    Adding inline-command arguments

--- test.py
+++ test.py
@@ -5,28 +5,13 @@ Copyright 2022 © Centre Interdisciplinaire de développement en Cartographie de
 
 from Sentinel2 import Sentinel2
 from hashlib import md5
+from settings import *
 import requests
 import pytest
 import sys
 import os
 
-
-######### PASSING ARGUMENTS IN PYTEST BY COMMAND-LINE #########
-
-from pytest import fixture
-
-def pytest_addoption(parser):
-    parser.addoption("--password", action="store")
-
-@fixture()
-def name(request):
-    return request.config.getoption("--password")
-    
-###############################################################
-
-
-# Create the satellite and select a footprint
-sat = Sentinel2('samueldubos', password)
+# Set the right footprint
 example_footprint = 'POLYGON((' \
                     '-69.9 51.7,' \
                     '-57.2 51.7,' \
@@ -39,70 +24,74 @@ example_date = 'NOW-6DAYS'
 example_date_1 = 'NOW-5DAYS'
 example_date_2 = 'NOW-8DAYS'
 
-# Make use of the search method
-search = sat.search(example_date, 'NOW', example_footprint)
-search_1 = sat.search(example_date_1, 'NOW', example_footprint)
-search_2 = sat.search(example_date_2, 'NOW', example_footprint)
-
 # Define a product
-product = '72574aaf-85f0-42f3-814a-ef08a079490a'
-
-# Download a product
-sat.download(product)      
+product = '72574aaf-85f0-42f3-814a-ef08a079490a'   
 
 
 # Unit tests concerning the Sentinel2 class
 class TestGeneral:
 
-    def test_status_code(self):
-        date = 'NOW-1DAYS'
-        username = sat.username
-        password = sat.password
-        req = requests.get(f'https://scihub.copernicus.eu/dhus/search?'
-                           f'q=(beginPosition:[{date}%20TO%20NOW])',
-                           auth=(username, password))
-        assert req.status_code == 200
+    def test_status_code(self, username, password):
+    	date = 'NOW-1DAYS'
+    	req = requests.get(f'https://scihub.copernicus.eu/dhus/search?'
+    	f'q=(beginPosition:[{date}%20TO%20NOW])',
+    	auth=(username, password))
+    	assert req.status_code == 200
 
 
 # Unit tests concerning the search() method
 class TestSearchMethod:
 
     @pytest.mark.parametrize("begin_date, end_date", [('NOW-0DAYS', 'NOW')])
-    def test_null_request_handled(self, begin_date, end_date):
+    def test_null_request_handled(self, begin_date, end_date, username, password):
+        sat = Sentinel2(username, password)
         self.begin_date = begin_date
         self.end_date = end_date
         sat.search(self.begin_date, self.end_date, example_footprint)
 
     @pytest.mark.parametrize("begin_date, end_date", [('2022-05-09T00:00:00.000Z', '2022-05-10T00:00:00.000Z')])
-    def test_unit_request_handled(self, begin_date, end_date):
+    def test_unit_request_handled(self, begin_date, end_date, username, password):
+        sat = Sentinel2(username, password)
         self.begin_date = begin_date
         self.end_date = end_date
         sat.search(self.begin_date, self.end_date, example_footprint)
-
-    def test_is_list_of_strings(self):
-        for el in search:
+        
+    def test_is_list_of_strings(self, username, password):
+        sat = Sentinel2(username, password)
+        for el in sat.search(example_date, 'NOW', example_footprint):
             assert isinstance(el, str)
-
-    def test_are_lengths_coherent(self):
+        
+    def test_are_lengths_coherent(self, username, password):
+        sat = Sentinel2(username, password)
+        search_1 = sat.search(example_date_1, 'NOW', example_footprint)
+        search_2 = sat.search(example_date_2, 'NOW', example_footprint)
         assert len(search_2) >= len(search_1)
 
     @pytest.mark.parametrize("begin_date, end_date", [('2022-05-15T00:00:00.000Z', '2022-05-17T00:00:00.000Z')])
-    def test_answer_uncluded(self, begin_date, end_date):
+    def test_answer_uncluded(self, begin_date, end_date, username, password):
+        sat = Sentinel2(username, password)
         self.begin_date = begin_date
         self.end_date = end_date
+        sat.search(self.begin_date, self.end_date, example_footprint)
         assert product in sat.search(self.begin_date, self.end_date, example_footprint)
 
-    
+
 # Unit tests concerning the download() method
 class TestDownloadMethod:
 
-    def test_file_exists(self):
+    def test_can_create_zip(self, username, password):
+        sat = Sentinel2(username, password)
+        download = sat.download(product)
+        
+    def test_file_exists(self, username, password):
+        sat = Sentinel2(username, password)
+        download = sat.download(product)
         path = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
         assert os.path.exists(f"{path}/{product}.zip")
-
-    def test_identical_binaries(self):
-        username = sat.username
-        password = sat.password
+    
+    def test_identical_binaries(self, username, password):
+        sat = Sentinel2(username, password)
+        download = sat.download(product)
         r = requests.get(f"https://scihub.copernicus.eu/dhus"
                          f"/odata/v1/Products('{product}')"
                          f"/$value",
